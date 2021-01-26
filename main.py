90f1991de784a1435158f1d8bd7018c374ab8a2d
Petr Gallus <75473928+PetrGallus@users.noreply.github.com>
2021-01-26 11:09:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 from Block import Block
+from datetime import datetime
 
 blockchain = []
 mempool = []
 
-genesis_block = Block ("Univerzita Obrany", ["3. ceta je na pozici",
-                                             "Pripravte se k provedeni lecky.",
-                                             "Rozumim, provedu"])
+#pro nasledne zobrazeni aktualniho casu ve formatu H:M:S:MS (milisekundy zaokrouhle na 3 mista)
+now = datetime.now()
+actual_time = now.strftime("%H:%M:%S.%f")[:-3]
 
-second_block = Block(genesis_block.block_hash, ["Doslo ke kontaktu s nepritelem.",
-                                                "Hlaste stav jednotky.",
-                                                "Dva zraneni, pozadujeme podporu."])
+#naplneni dat do jednotlivych bloku
 
-third_block = Block(second_block.block_hash, ["Rozkaz velitele 3. roty cislo 127 ze dne...",
-                                              "Seznam osob s bezpecnostni proverkou stupne Tajne:...",
-                                              "Velitelem 7. ukoloveho uskupeni rotace Bravo..."])
+genesis_block = Block("Univerzita Obrany", ["Foxtrot: 3. ceta je na pozici",
+                                            "Alpha:   Pripravte se k provedeni lecky.",
+                                            "Foxtrot: Rozumim, provedu"])
 
+second_block = Block(genesis_block.block_hash, ["Foxtrot: Doslo ke kontaktu s nepritelem.",
+                                                "Alpha:   Hlaste stav jednotky.",
+                                                "Foxtrot: Dva zraneni, pozadujeme medevac."])
+
+third_block = Block(second_block.block_hash, ["Rozkaz velitele 3. roty cislo 127 ze dne 1.4.2021..."])
+
+fourth_block = Block(third_block.block_hash, ["Aktualni seznam osob s bezpecnostni proverkou stupne Tajne:..."])
+
+fifth_block = Block(fourth_block.block_hash, ["Velitelem 7. ukoloveho uskupeni rotace Bravo..."])
+
+
+#zobrazeni jednotlivych bloku spolecne s aktualnim casem a hashem - kazdy jednotlivy hash je zavisly na datech sveho predchoziho bloku
 print("/Genesis Block\\")
 print("Block hash: ",genesis_block.block_hash)
+print("Time: ", actual_time)
 
-print("\n/Second Block\\")
+print("\n/Block #2\\")
 print("Block hash: ",second_block.block_hash)
+print("Time: ", actual_time)
 
-print("\n/Third Block\\")
+print("\n/Block #3\\")
 print("Block hash: ",third_block.block_hash)
+print("Time: ", actual_time)
+
+print("\n/Block #4\\")
+print("Block hash: ",fourth_block.block_hash)
+print("Time: ", actual_time)
+
+print("\n/Block #5\\")
+print("Block hash: ",fifth_block.block_hash)
+print("Time: ", actual_time)
 
 #nefunguje
-print(Block)
+print("\n",Block)
